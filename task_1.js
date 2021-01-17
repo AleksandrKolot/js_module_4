d2b6e9a287596975cb13f12440647288691ad00f
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2021-01-17 13:45:07 +0200

    4-1

---
+++ task_1.js
@@ -0,0 +1,16 @@
+const addIndex = (element, index) => element + index;
+
+const subIndex = (element, index) => element - index;
+
+function mapArray(array, cb) {
+  'usen strict';
+  const numbers = new Array(array.length);
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+    numbers[i] = cb(element, index);
+  }
+  return numbers;
+}
+const arr = [1, 2, 3, 4, 5];
+console.log(mapArray(arr, addIndex));
